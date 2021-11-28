ce02045c819ee5e85c9127abb4bd2f45ec85f2cf
dtalmach <Dmytro.Talmach@harmonicinc.com>
2021-11-29 00:06:29 +0200

    lab2: partition text

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -10,3 +10,29 @@ def format_text(file_name):
 def read_formatted_text(file_name):
 	with open(file_name, 'r') as rf:
 		return rf.read()
+
+def cut_to_non_overlapping_text_pieces(input_text, piece_length, num_of_pieces) -> list:
+	result = []
+	for i in range(num_of_pieces):
+		cur_offset = i * piece_length
+		text_piece = input_text[cur_offset:cur_offset+piece_length]
+		if text_piece:
+			result.append(text_piece)
+	return result
+
+
+
+if __name__ == '__main__':
+
+	random.seed(1)
+
+	text = read_formatted_text('output_file')
+	
+	L_and_N = [
+		(10,    10000),
+		(100,   10000),
+		(1000,  10000),
+		(10000, 1000)			# maybe need 10 million chars text for this? now have only 1 mill
+	]
+
+	text_pieces = list(map(lambda x: cut_to_non_overlapping_text_pieces(text, *x), L_and_N))
