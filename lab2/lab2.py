20b762fe38cebcb5e828dfbc5181beaa2b5e34eb
dtalmach <Dmytro.Talmach@harmonicinc.com>
2021-11-29 00:07:46 +0200

    lab2: add text distortion methods

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -11,6 +11,75 @@ def read_formatted_text(file_name):
 	with open(file_name, 'r') as rf:
 		return rf.read()
 
+def distort_text_pieces(method: str, input_texts: list) -> str:
+	
+	ALPH = 'абвгдеєжзиіїйклмнопрстуфхцчшщьюя'
+	c_to_v_mapping = dict(zip(ALPH, itertools.count(0)))
+	v_to_c_mapping = {v: k for k, v in c_to_v_mapping.items()}
+
+	cc_to_v_mapping = dict(zip(map(lambda x: "".join(x), itertools.product(ALPH, repeat=2)), itertools.count(0)))
+	v_to_cc_mapping = {v: k for k, v in cc_to_v_mapping.items()}
+
+	def Vigenere(input_text):
+		result = ''
+
+		r = 5
+		key = "".join(random.choices(ALPH, k = r))
+
+		for i in range(len(input_text)):
+			result += v_to_c_mapping[(c_to_v_mapping[input_text[i]] + c_to_v_mapping[key[i%r]]) % len(ALPH)]
+		return result
+
+	def affine_subst_mono(input_text):
+		result = ''
+		a = random.randint(1, len(ALPH)-1)
+		b = random.randint(0, len(ALPH)-1)
+
+		for i in range(len(input_text)):
+			result += v_to_c_mapping[(a * c_to_v_mapping[input_text[i]] + b) % len(ALPH)]
+		return result
+
+	def affine_subst_bi(input_text):
+		result = ''
+		a = random.randint(1, len(ALPH)**2-1)
+		b = random.randint(0, len(ALPH)**2-1)
+
+		for i in range(0, len(input_text), 2):
+			result += v_to_cc_mapping[(a * cc_to_v_mapping[input_text[i:i+2]] + b) % len(ALPH)**2]
+		return result
+
+	def uniform_mono(input_text):
+		return [v_to_c_mapping[round(random.uniform(0, len(ALPH)-1))] for i in range(len(input_text))]
+
+	def uniform_bi(input_text):
+		return [v_to_cc_mapping[round(random.uniform(0, len(ALPH)**2-1))] for i in range(0, len(input_text), 2)]
+
+	def g_mono(input_text):
+		y0 = random.randint(1, len(ALPH)-1)
+		y1 = random.randint(0, len(ALPH)-1)
+		y = [y0, y1]
+
+		for i in range(2, len(input_text)):
+			y.append((y[-1]+y[-2])%len(ALPH))
+
+		return "".join(map(lambda x: v_to_c_mapping[x], y))
+
+	def g_bi(input_text):
+		y0 = random.randint(1, len(ALPH)**2-1)
+		y1 = random.randint(0, len(ALPH)**2-1)
+		y = [y0, y1]
+
+		for i in range(2, len(input_text), 2):
+			y.append((y[-1]+y[-2])%len(ALPH)**2)
+
+		return "".join(map(lambda x: v_to_cc_mapping[x], y))
+
+	result = []
+	#result = list(map(locals()[method], input_texts))
+	result = list(zip(input_texts, map(locals()[method], input_texts)))
+
+	return result
+
 def cut_to_non_overlapping_text_pieces(input_text, piece_length, num_of_pieces) -> list:
 	result = []
 	for i in range(num_of_pieces):
@@ -19,8 +88,6 @@ def cut_to_non_overlapping_text_pieces(input_text, piece_length, num_of_pieces) 
 		if text_piece:
 			result.append(text_piece)
 	return result
-
-
 
 if __name__ == '__main__':
 
@@ -36,3 +103,13 @@ if __name__ == '__main__':
 	]
 
 	text_pieces = list(map(lambda x: cut_to_non_overlapping_text_pieces(text, *x), L_and_N))
+
+	text_pieces = list(map(lambda x: cut_to_non_overlapping_text_pieces(text, *x), L_and_N))
+	#print(f"text_len = {len(text)}, th's = {len(r[2])}, h of th's = {len(r[3])}")
+	#print(list(map(len, r[3])))
+	# print(r)
+	
+	print(distort_text_pieces('Vigenere', text_pieces[1]))
+	# print(distort_text_pieces('affine_subst_mono', text_pieces[0]))
+	print(distort_text_pieces('uniform_bi', text_pieces[0]))
+	print(distort_text_pieces('g_bi', text_pieces[0]))
