afad3d8fd28f595f35a424093ff4110bc91c6979
Teh Absqua <absqua.a@gmail.com>
2021-12-20 21:03:59 +0200

    chinese remainder

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -11,3 +11,11 @@ def gcd (a, b) :
         v, v0 = v0, v - v0 * q
         u, u0 = u0, u - u0 * q
     return (a, u, v)
+
+def chineserem (lC, ln, Me) :
+    n = np.prod(ln)
+    lN = [n / ni for ni in n]
+    lM = [gcd(Ni, ni)[2] for Ni, ni in zip(lN, ln)]
+    C = np.prod([Me * Ni * Mi for Ni, Mi in zip(lN, lM)])
+    return C
+
